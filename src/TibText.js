c2f6b0ce0f74c469d7551dfb424c9603186293f1
K1000 <selwak@gmail.com>
2017-08-27 21:35:08 +0200

    init

--- src/TibText.js
+++ src/TibText.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import Find from './lib/Find.js'
+// import Find from './lib/Find.js'
 import Syllabe from './Syllabe.js'
+import axios from 'axios';
 
+const syllabesURL = 'https://sheets.googleapis.com/v4/spreadsheets/1D6NW7phdjwmz7bnncNgJcwNVgwn39SsOCVvZ403VilE/values/syllabes-unique!A2%3AC4056?key=AIzaSyCSZo1p3NxY73vcsDo554y3chNSTp4uhqY'
 
 class tibText extends Component {
   constructor(props) {
     super(props);
     this.text = props.text
-    console.log(props.text)
     this.textArray = []
+    this.syllabes = []
   }
-
+  componentDidMount() {
+    axios.get(syllabesURL)
+      .then(res => {
+        this.syllabes = res.data.values;
+      });
+  }
   toSyllabes(text){
     if (text.endsWith('་'))
       text = `${text.slice(0, -1)}་`
     this.textArray = text.split('་')
     return this.textArray.map((s, i) => this.renderSylabe(s, i))
   }
-
+  search(string) {
+      string = string.trim()
+    if (string.endsWith('།'))
+      string = `${string.slice(0, -1)}་`
+    if (!string.endsWith('་'))
+      string = `${string}་`
+    let rows = this.syllabes;
+    for(var row in rows){
+      if (rows[row][0] === string) {
+        let res = rows[row]
+        return {tib: res[0], wy: res[1], spel: res[2], dra: res[3]}
+      }
+    }
+    return false
+  }
   renderSylabe(s, i) {
-    var find = Find(s, 0)
+    var find = this.search(s)
     if (s === '')
     	return ''
     let tib = `${this.textArray[i]}་`
