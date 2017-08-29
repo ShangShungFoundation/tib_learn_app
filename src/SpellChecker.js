d8b1065c320eec870c69887b0927bf056c5706f9
K1000 <selwak@gmail.com>
2017-08-29 09:27:00 +0200

    get query from url

--- src/SpellChecker.js
+++ src/SpellChecker.js
@@ -6,20 +6,23 @@ import TibText from './TibText.js'
 class SpellChecker extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {query: ''}
+		let query = decodeURIComponent(this.props.location.search).substring(1)
+		this.state = {query: query}
 	}
 	doSpell = (e) => {
+		e.preventDefault();
     	this.setState({query: e.currentTarget.value})
+
 	}
 	render() {
-		const query = this.state.query
+		const query = this.state.query.trim()
 		const isQuery = (this.state.query !== '')
 		return(
 			<div className="search">
 				<h2>Tibetan spell check</h2>
 				<p className="info">Copy/Paste line of tibetan text into the box below</p>
 				<p className="query">
-					<input type="text" placeholder="Tib. text to spellcheck" onChange={this.doSpell} className="queryInput"/>
+					<input type="text" placeholder="Tib. text to spellcheck" onKeyPress={this.doSpell} defaultValue={query} className="queryInput"/>
 				</p>
 
 				{isQuery && <TibText text={query} /> }
